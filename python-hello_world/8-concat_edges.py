002518168eae3d7c3929a8759a0fe27cdbc5b844
izzeddin62 <i.ishimwe@alustudent.com>
2022-06-29 04:19:22 -0700

    remove one line

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -3,4 +3,3 @@ str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(str[39:66], end=' ')
 print(str[107:111],end=' ')
-print(str[:6])
