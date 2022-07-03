f246e84a2e215051c1ab17951229b681d94f3e0c
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-03 06:36:18 -0700

    added the 2-args.py script

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print('0 arguments.')
+    else:
+        print('{} argument:'.format(len(sys.argv)))
+        for i in range(len(sys.argv)):
+            if i > 0:
+                print('{}: {}'.format(i, sys.argv[i]))
