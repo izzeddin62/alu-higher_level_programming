b857d87944b68de469f075528b31ebf947e770ea
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-03 06:40:44 -0700

    modified the return string when there is only one argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,8 +3,11 @@ if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
         print('0 arguments.')
-    else:
+    elif len(sys.argv) - 1 == 1:
         print('{} argument:'.format(len(sys.argv) - 1))
+        print('{}: {}'.format(1, sys.argv[1]))
+    else:
+        print('{} arguments:'.format(len(sys.argv) - 1))
         for i in range(len(sys.argv)):
             if i > 0:
                 print('{}: {}'.format(i, sys.argv[i]))
