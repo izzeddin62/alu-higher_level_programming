e86658e2d3829deae6cbbd7b611e93a6d56e3509
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-03 06:38:09 -0700

    modified 2-args.py script

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print('0 arguments.')
     else:
-        print('{} argument:'.format(len(sys.argv)))
+        print('{} argument:'.format(len(sys.argv) - 1))
         for i in range(len(sys.argv)):
             if i > 0:
                 print('{}: {}'.format(i, sys.argv[i]))
