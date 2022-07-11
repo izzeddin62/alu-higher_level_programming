8c18632461b5f017a6c0aa945fd687b41fb5da54
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-11 15:07:24 -0700

    print a new line when the matrix in empty

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,11 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in matrix:
-            for j in i:
-                if i.index(j) == len(i) - 1:
-                    print('{:d}'.format(j))
-                    break
-                print('{:d}'.format(j), end=" ")
+            if i:
+                for j in i:
+                    if i.index(j) == len(i) - 1:
+                        print('{:d}'.format(j))
+                        break
+                    print('{:d}'.format(j), end=" ")
+            else:
+                print()
