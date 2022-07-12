22f20d0db2ec9945c1b3cf4a39134c196682d172
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-12 04:40:05 -0700

    remove the builtin max function

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) > 0:
-        return max(my_list)
+        my_list.sort()
+        return my_list[-1]
     else:
         return None
