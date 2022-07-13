8c6313434d2dbbfe7701d456f9525efc2cabc1e0
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-13 10:38:29 -0700

    return 0 if the argument is not a string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not type(roman_string) == str or not roman_string:
-        return None
+        return 0
     roman_numbers = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500,
                      "M": 1000, "IV": 4, "IX": 9, "XL": 40, "XC": 90,
                      "CD": 400, "CD": 900}
@@ -10,7 +10,7 @@ def roman_to_int(roman_string):
     while i < len(roman_string):
         if roman_string[i:i+2] in sorted(roman_numbers):
             integer_number = integer_number + \
-            roman_numbers[roman_string[i:i+2]]
+                roman_numbers[roman_string[i:i+2]]
             i = i + 2
         else:
             integer_number = integer_number + roman_numbers[roman_string[i]]
