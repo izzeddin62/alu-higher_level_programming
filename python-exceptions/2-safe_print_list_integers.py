2da1fd6969dfedbadf2f10bd73702368cbcb11eb
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-18 23:45:03 +0200

    added the 2-safe_print_list_integers.py script

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        i = 0
+        list_string = ""
+        while i <= x - 1:
+            try:
+                list_string += "{:d}".format(my_list[i])
+                i += 1
+            except (TypeError, ValueError):
+                i += 1
+                pass
+        print(list_string)
+        length = 0
+        for n in list_string:
+            length += 1
+        return length
+    except IndexError:
+        raise
