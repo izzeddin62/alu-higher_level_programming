6785e0a838eec3eb8bd9c0f6ce4a814a33455d4c
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-19 18:26:25 +0200

    added the finally block

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,19 +3,19 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     i = 0
     while i < list_length:
+        x = 0
         try:
-            new_list.append(my_list_1[i] / my_list_2[i])
+            x = my_list_1[i] / my_list_2[i]
             i += 1
         except ZeroDivisionError:
             print("division by 0")
-            new_list.append(0)
             i += 1
         except TypeError:
             print("wrong type")
-            new_list.append(0)
             i += 1
         except IndexError:
             print("out of range")
-            new_list.append(0)
             break
+        finally:
+            new_list.append(x)
     return new_list
