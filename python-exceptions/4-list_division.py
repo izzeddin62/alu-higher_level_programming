0206a83669e0612558e7b6d8147c09e4497be688
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-19 18:28:10 +0200

    remove the break statement

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -15,7 +15,7 @@ def list_division(my_list_1, my_list_2, list_length):
             i += 1
         except IndexError:
             print("out of range")
-            break
+            i += 1
         finally:
             new_list.append(x)
     return new_list
