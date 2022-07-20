1e7cb4a35882ca725918d940d8b0e42d3b32daad
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-20 19:23:37 +0200

    check parameter before assignment in the size setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -33,4 +33,9 @@ class Square:
         Args:
             size (int): represent the new size
         """
-        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
