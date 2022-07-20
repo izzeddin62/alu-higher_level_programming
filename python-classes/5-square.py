304ab586daa66edb9d01005da8c277b39f5bbdac
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-20 19:37:14 +0200

    added the 5-square.py script

---
+++ python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""defines Square class"""
+
+
+class Square:
+    """It represents a square"""
+    def __init__(self, size=0):
+        """set the square size
+
+        Args:
+            size (int, optional): the square size measure
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """returns the area of the square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """get square property"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """set size property
+
+        Args:
+            size (int): represent the new size
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def my_print(self):
+        """shows the square area on the stdout using #"""
+        i = 0
+        while i < self.__size:
+            print("#" * self.__size)
+            i += 1
