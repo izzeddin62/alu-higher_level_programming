fc0bacd1cbc724f4438cfbe04598bd5a13eb81f7
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-20 18:56:04 +0200

    added the 1-square.py script

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defines Square class"""
+
+
+class Square:
+    """It represents a square"""
+    def __init__(self, size):
+        """set the square size
+
+        Args:
+            size (int): the square size measure
+        """
+        self.__size = size
