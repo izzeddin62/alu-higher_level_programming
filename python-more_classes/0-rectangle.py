03c752ca8d00fb759ec62109df3533f2edb6852d
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-21 23:47:45 +0200

    added the 0-rectangle.py script

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""contains an empty class Rectangle"""
+
+
+class Rectangle:
+    """describe a rectangle instance"""
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,3 @@
+# python - more classes
+### 0-rectangle.py
+defines an empty class Rectangle that defines a rectangle
