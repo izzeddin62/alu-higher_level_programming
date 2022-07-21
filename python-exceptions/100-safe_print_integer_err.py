47fbd4342ce7b9089c89d3a80c243aa07289d894
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-21 22:12:16 +0200

    added the TypeError to the except

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as err:
+    except (ValueError, TypeError) as err:
         sys.stderr.write(str(err) + "\n")
         return False
+
