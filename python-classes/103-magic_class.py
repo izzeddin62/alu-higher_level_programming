ae89180bb898b98ea93b50aa6f95f5a0155b2d75
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-21 21:17:53 +0200

    use != comparizon op

--- python-classes/103-magic_class.py
+++ python-classes/103-magic_class.py
@@ -7,7 +7,7 @@ import math
 class MagicClass:
     """creates circle instance"""
     def __init__(self, radius=0):
-        if radius is not int and radius is not float:
+        if type(radius) != int and type(radius) != float:
             raise TypeError("radius must be a number")
         self._MagicClass__radius = radius
 
