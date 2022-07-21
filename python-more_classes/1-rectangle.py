81e87b1282f61243f03641eb1b5ccc162fb117ba
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-22 00:04:54 +0200

    change function from int to type

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if int(value) != int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -24,7 +24,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if int(value) != int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
