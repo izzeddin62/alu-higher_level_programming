76e01440274c5120786d3ea0c22f126b6d627a93
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-26 20:57:18 +0200

    added the module documentation and class documentation

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""defines the LockedClass
+   
+   the LockedClass is a class that only allows attribute first_name
+""" 
+
+
 class LockedClass:
+    """LockedClass class with only first_name attribute"""
     __slots__ = "first_name"
