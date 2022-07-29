6d4a97850939e1b94f4537ba647f955db615ceb6
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-29 21:31:49 +0200

    added Origin Header

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # return You got me! in rhe response body
-curl -sL -X PUT -d "user_id=98" "0.0.0.0:5000/catch_me"
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
