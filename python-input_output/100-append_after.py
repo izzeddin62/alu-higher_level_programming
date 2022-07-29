b1929aa618e1d88c03c161571eba1870562c5a58
izzeddin62 <i.ishimwe@alustudent.com>
2022-07-29 12:33:46 +0200

    modify 100-append_after.py script

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -7,13 +7,9 @@ def append_after(filename="", search_string="", new_string=""):
     and save it in a file"""
     new_text = ""
     with open(filename, "r", encoding="utf-8") as f:
-        new_text = f.read()
-        f.seek(0)
         for line in f:
+            new_text += line
             if search_string in line:
-                pos = new_text.find(line)
-                insert_pos = new_text[:pos + len(line)]
-                end_pos = new_text[pos + len(line):]
-                new_text = insert_pos + new_string + end_pos
+                new_text += new_string
     with open(filename, "w", encoding="utf-8") as j:
         j.write(new_text)
