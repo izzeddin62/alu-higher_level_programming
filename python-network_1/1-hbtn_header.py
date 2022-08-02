15eb61ac140efdb73f802633bd260efd04d9c05a
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-03 01:29:57 +0200

    check if the file is imported

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -4,5 +4,6 @@
 import urllib.request
 import sys
 
-with urllib.request.urlopen(sys.argv[1]) as res:
-    print(res.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        print(res.headers.get('X-Request-Id'))
