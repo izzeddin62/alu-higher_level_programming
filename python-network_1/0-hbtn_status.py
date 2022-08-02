99aa819376acf696e3be5b5faa7251d7373fac33
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-03 00:27:59 +0200

    added 0-hbtn_status.py script

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""the script get the status from intranet.hbtn.io"""
+import urllib.request
+
+with urllib.request.urlopen("https://intranet.hbtn.io/status") as res:
+    body = res.read()
+    print("Body response:")
+    print(f"- type: {type(body)}")
+    print(f"- content: {body}")
+    print(f"- utf8 content: {body.decode('utf-8')}")
---
+++ python-network_1/README.md
@@ -0,0 +1,3 @@
+# Python network - 1
+### 0-hbtn_status.py
+a Python script that fetches https://intranet.hbtn.io/status
