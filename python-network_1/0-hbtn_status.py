72abe0e9ef8d778c036c5727cfcd48eb945cc082
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-03 00:36:52 +0200

    added if statement

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,9 +3,10 @@
 
 import urllib.request
 
-with urllib.request.urlopen("https://intranet.hbtn.io/status") as res:
-    body = res.read()
-    print("Body response:")
-    print(f"- type: {type(body)}")
-    print(f"- content: {body}")
-    print(f"- utf8 content: {body.decode('utf-8')}")
+if __name__ = "__main__"
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as res:
+        body = res.read()
+        print("Body response:")
+        print(f"- type: {type(body)}")
+        print(f"- content: {body}")
+        print(f"- utf8 content: {body.decode('utf-8')}")
