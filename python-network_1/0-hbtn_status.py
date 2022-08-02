0125bb6c7f94f5991ce78933e35d4fef2e8b9d0d
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-03 01:12:23 +0200

    remove some comments

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""fetch data, get the status of a website using url and display the statu"""
+"""fetch data, get the status of a website using url and display the status"""
 
 
 import urllib.request
 
-
-"""send a get request"""
 if __name__ == "__main__":
-    """use urllib.request to send an request to the url"""
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as res:
         body = res.read()
         print("Body response:")
