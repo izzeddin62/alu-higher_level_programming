2caff241b663278d6ac130687f2dbeec2d18a170
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-03 01:00:03 +0200

    added comments

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,6 +5,7 @@ import urllib.request
 
 """send a get request"""
 if __name__ == "__main__":
+    """use urllib.request to send an request to the url"""
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as res:
         body = res.read()
         print("Body response:")
