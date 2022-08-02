d868441f171ac18a647754e49eba9d8ad2ee234a
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-03 01:47:52 +0200

    added tabs

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as res:
         body = res.read()
         print("Body response:")
-        print(f"- type: {type(body)}")
-        print(f"- content: {body}")
-        print(f"- utf8 content: {body.decode('utf-8')}")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
