5d5b8ff912e4db1a79e53aa92a61e8dd28780994
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-03 15:57:20 +0200

    modify url

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -5,7 +5,7 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = "/http://0.0.0.0:5000/search_user"
+    url = "http://0.0.0.0:5000/search_user"
     body = {'q': ""}
     if len(sys.argv) > 1:
         body['q'] = sys.argv[1]
