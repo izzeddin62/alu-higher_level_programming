3264f4d2b72e6975195176a5f7a9cdbfae838207
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-03 14:35:00 +0200

    use format string method on 4-hbtn_status.py script

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == "__main__":
     res = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print(f"\t - type: {type(res.text)}")
-    print(f"\t - content: {res.text}")
+    print("\t - type: {}".format(type(res.text)))
+    print("\t - content: {}".format(res.text))
