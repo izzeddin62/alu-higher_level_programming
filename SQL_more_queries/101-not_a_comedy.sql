fa9d301c6ad46f700876ffe3cbb8031a47f1078a
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-05 19:49:43 +0200

    add tvshow with not genre

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -3,7 +3,7 @@ SELECT DISTINCT tv_shows.title
 FROM tv_genres
 JOIN tv_show_genres 
 ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows
+RIGHT JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title NOT IN (SELECT DISTINCT tv_shows.title
 FROM tv_genres
