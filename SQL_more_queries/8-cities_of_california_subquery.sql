3d8160fc7bb93b62114ce9667a6d753c6929957d
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-05 16:47:07 +0200

    only list id and name

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- list cities in California
-SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California');
+SELECT id,name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California');
