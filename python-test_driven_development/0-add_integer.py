6e9aa7f98974fffe67ab8eb7de007538bf9a4d6e
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-08 13:38:01 +0200

    test nan float

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,6 +2,7 @@
 """addition function module"""
 
 def add_integer(a, b=98):
+    """add two number together"""
     if type(a) != int and type(a) != float:
         raise TypeError('a must be an integer')
     elif type(b) != int and type(b) != float:
