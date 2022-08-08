88935864365face49cb66b9528ff9ec92478d667
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-08 15:17:24 +0200

    return nothing when the size is zero

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -9,7 +9,7 @@ def print_square(size):
     elif size < 0:
         raise ValueError("size must be >= 0")
     if size == 0:
-        print("")
+        return
     else:
         i = 0
         while i < size:
