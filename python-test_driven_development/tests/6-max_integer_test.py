e333483aaacb5778ce84e97c23210fae7c5ff493
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-08 17:41:12 +0200

    add more test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,5 +10,10 @@ class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
         """test the function when the input is correct"""
         self.assertAlmostEqual(max_integer([1,2,3,4]), 4)
+        self.assertAlmostEqual(max_integer([5,2,3,4]), 5)
+        self.assertAlmostEqual(max_integer([1,2, 7 ,3,4]), 7)
+        self.assertAlmostEqual(max_integer([1,2,3,-4]), 3)
+        self.assertAlmostEqual(max_integer([4]), 4)
+        self.assertAlmostEqual(max_integer([-4]), -4)
         self.assertAlmostEqual(max_integer([]), None)
         self.assertAlmostEqual(max_integer(), None)
