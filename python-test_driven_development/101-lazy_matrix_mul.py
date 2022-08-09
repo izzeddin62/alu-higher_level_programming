94c1745d7e1ec76ea8c7beadbec5a8b6908aa8b9
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-09 22:04:21 +0200

    changed the returned data

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -43,4 +43,4 @@ def lazy_matrix_mul(m_a, m_b):
             raise TypeError("each row of m_b must be of the same size")
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
-    return np.matmul(m_a, m_b).tolist() 
+    return np.matmul(m_a, m_b) 
