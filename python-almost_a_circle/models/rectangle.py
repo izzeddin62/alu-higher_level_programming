cb948f7c88e55ce8c594fe839756826ca829bd53
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-10 14:58:25 +0200

    check if x and y are greater than zero

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -48,6 +48,8 @@ class Rectangle(Base):
     def x(self, value):
         if type(value) != int:
             raise TypeError("x must be an integer")
+        elif value <= 0:
+            raise ValueError("x must be > 0")
         else:
             self.__x = value
 
@@ -59,5 +61,7 @@ class Rectangle(Base):
     def y(self, value):
         if type(value) != int:
             raise TypeError("y must be an integer")
+        elif value <= 0:
+            raise ValueError("y must be > 0")
         else:
             self.__y = value
