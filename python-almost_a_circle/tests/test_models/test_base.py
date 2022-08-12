12a28d794df4c7810b23509bea7ccdb54add77af
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-12 11:48:53 +0200

    change test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -4,10 +4,8 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
     def test_initialization(self):
-        obj_1 = Base(2)
-        obj_2 = Base()
-        self.assertEqual(obj_1.id, 2)
-        self.assertEqual(obj_2.id, 1)
+        base = Base()
+        self.assertEqual(base.id, 1)
 
 
 if __name__ == "__main__":
