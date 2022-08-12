76daf8bb5ae36ee0b3dcb75cd1c505fed860db1b
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-12 14:39:18 +0200

    added to_json_string test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -14,5 +14,14 @@ class TestBase(unittest.TestCase):
         base = Base(34)
         self.assertEqual(base.id, 34)
 
+    def test_json_string(self):
+        base = Base(2)
+        with_none = Base.to_json_string(None)
+        empty_list = Base.to_json_string([])
+        list_el = Base.to_json_string([{"id": 1}])
+        self.assertEqual(with_none, "[]")
+        self.assertEqual(empty_list, "[]")
+        self.assertEqual(list_el, '[{"id": 1}]')
+
 if __name__ == "__main__":
     unittest.main()
