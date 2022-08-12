5bd11d231e059881072e7776429b9d11b3bc8839
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-12 16:03:37 +0200

    test when x and y are not integers

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -21,7 +21,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rect_3.x, 8)
         self.assertEqual(rect_3.y, 4)
 
-    def test_wrong_argument_type(self):
+    def test_no_position_argument_type(self):
         with self.assertRaises(TypeError) as error:
             Rectangle()
         message = "__init__() missing 2 required positional arguments: 'width' and 'height'"
@@ -34,6 +34,8 @@ class TestRectangle(unittest.TestCase):
             Rectangle(4, 4, 5, 6, 7, 8)
         message = "__init__() takes from 3 to 6 positional arguments but 7 were given"
         self.assertEqual(str(error.exception), message)
+
+    def test_invalid_width(self):
         with self.assertRaises(TypeError) as error:
             Rectangle("3", 2)
         message = "width must be an integer"
@@ -42,6 +44,8 @@ class TestRectangle(unittest.TestCase):
             Rectangle(-4, 4)
         message = "width must be > 0"
         self.assertEqual(str(error.exception), message)
+
+    def test_invalid_height(self):
         with self.assertRaises(TypeError) as error:
             Rectangle(3, "2")
         message = "height must be an integer"
@@ -50,11 +54,23 @@ class TestRectangle(unittest.TestCase):
             Rectangle(4, -4)
         message = "height must be > 0"
         self.assertEqual(str(error.exception), message)
+
+    def test_invalid_x(self):
         with self.assertRaises(ValueError) as error:
             Rectangle(4, 4, -3)
         message = "x must be >= 0"
         self.assertEqual(str(error.exception), message)
+        with self.assertRaises(TypeError) as error:
+            Rectangle(4, 4, "4")
+        message = "x must be an integer"
+        self.assertEqual(str(error.exception), message)
+
+    def test_invalid_y(self):
         with self.assertRaises(ValueError) as error:
             Rectangle(4, 4, 5, -6)
         message = "y must be >= 0"
+        self.assertEqual(str(error.exception), message)
+        with self.assertRaises(TypeError) as error:
+            Rectangle(4, 4, 5, 'er')
+        message = "y must be an integer"
         self.assertEqual(str(error.exception), message)
