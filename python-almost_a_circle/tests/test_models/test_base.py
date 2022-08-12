d65bd02bb733a391d869fc68c4276d02d19b0733
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-12 12:56:05 +0200

    add more assertions

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -6,9 +6,10 @@ class TestBase(unittest.TestCase):
     def test_initialization(self):
         """test when not id is passed the base is initialized with the class id"""
         base = Base()
+        base2 = Base()
         self.assertEqual(str(type(base)), "<class 'models.base.Base'>")
         self.assertEqual(base.id, 1)
-
+        self.assertEqual(base2.id, 2)
 
 if __name__ == "__main__":
     unittest.main()
