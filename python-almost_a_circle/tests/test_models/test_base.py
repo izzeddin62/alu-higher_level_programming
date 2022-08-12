1b708f89c25182caa89881cc97ddff385bba979b
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-12 12:44:52 +0200

    add comments

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -4,7 +4,9 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
     def test_initialization(self):
+        """test when not id is passed the base is initialized with the class id"""
         base = Base()
+        self.assertEqual(str(type(base)), "<class 'models.base.Base'>")
         self.assertEqual(base.id, 1)
 
 
