5106249df77c34ad5215024dc86887ac79efa859
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-25 13:42:49 +0200

    resolve pep8 issue

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,8 +6,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """State model class for the states table"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
