ee4c7b1e98437f4ac67192986f4824094378c717
izzeddin62 <i.ishimwe@alustudent.com>
2022-08-25 14:21:04 +0200

    resolve pep8 issues

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -14,5 +14,7 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).filter(State.name.like('%a%')).order_by(State.id).all():
+    states = session.query(State)\
+        .filter(State.name.like('%a%')).order_by(State.id).all()
+    for state in states:
         print("{}: {}".format(state.id, state.name))
