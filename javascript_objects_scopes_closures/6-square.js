4da4a075391c364a91e0f0cf0fc8412eaa315c11
izzeddin62 <i.ishimwe@alustudent.com>
2022-10-06 22:58:07 +0200

    added 6-square.js

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      console.log((c || 'X').repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
