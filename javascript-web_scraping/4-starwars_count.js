8c9d3c06f20a3658a725e8d0eae1ae73f30a9df5
izzeddin62 <i.ishimwe@alustudent.com>
2022-10-10 23:24:59 +0200

    changed 4th script

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,5 +2,5 @@
 const request = require('request');
 request.get(process.argv[2], (err, response, body) => {
   if (err) console.log(err);
-  console.log(JSON.parse(body).results.filter(el => el.characters.includes('https://swapi-api.hbtn.io/api/people/18/')).length);
+  console.log(JSON.parse(body).results.filter(el => el.characters.find(character => character.includes('18'))).length);
 });
