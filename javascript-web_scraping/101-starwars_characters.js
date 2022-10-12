6a39c19873581f2613c05ad36308aae53b9e347a
izzeddin62 <i.ishimwe@alustudent.com>
2022-10-12 18:54:23 +0200

    changed 101 script

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -2,10 +2,16 @@
 const request = require('request');
 request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
   if (err) console.log(err);
-  JSON.parse(body).characters.forEach(el => {
-    request.get(el, (error, res, data) => {
-      if (error) console.log(err);
-      console.log(JSON.parse(data).name);
-    });
-  });
+  logCharacter(JSON.parse(body).characters, 0);
 });
+
+function logCharacter (characters, i) {
+  if (i > characters.length - 1) {
+    return;
+  }
+  request.get(characters[i], (error, res, data) => {
+    if (error) console.log(err);
+    console.log(JSON.parse(data).name);
+    logCharacter(characters, i + 1);
+  });
+}
